8cfd9b7ec1cd268da3c7f54e498bc0664241d2fb
jahongir <atametov97@mail.ru>
2023-02-01 01:05:37 +0500

    RateRepository is added!

---
+++ src/main/java/com/jahongir/mini_transaction/repository/RateRepository.java
@@ -0,0 +1,13 @@
+package com.jahongir.mini_transaction.repository;
+
+import com.jahongir.mini_transaction.domains.Rate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author jahongir
+ * @created 01/02/23 - 01:04
+ * @project Mini_transaction/IntelliJ IDEA
+ */
+
+public interface RateRepository extends JpaRepository<Rate, Long> {
+}
