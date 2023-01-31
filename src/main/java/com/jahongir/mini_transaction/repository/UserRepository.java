3ab64261829e6bb7988ffc33f125a2b31948d0f9
jahongir <atametov97@mail.ru>
2023-02-01 00:46:13 +0500

    UserRepository is added!

---
+++ src/main/java/com/jahongir/mini_transaction/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.jahongir.mini_transaction.repository;
+
+import com.jahongir.mini_transaction.domains.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+/**
+ * @author jahongir
+ * @created 31/01/23 - 23:43
+ * @project Mini_transaction/IntelliJ IDEA
+ */
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
