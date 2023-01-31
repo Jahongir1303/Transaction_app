44e9ced8e7d443ad6f4174d63147d458f0b0dc69
jahongir <atametov97@mail.ru>
2023-02-01 01:01:22 +0500

    CardRepository is added!

---
+++ src/main/java/com/jahongir/mini_transaction/repository/CardRepository.java
@@ -0,0 +1,13 @@
+package com.jahongir.mini_transaction.repository;
+
+import com.jahongir.mini_transaction.domains.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author jahongir
+ * @created 01/02/23 - 01:00
+ * @project Mini_transaction/IntelliJ IDEA
+ */
+
+public interface CardRepository extends JpaRepository<Card, Long> {
+}
