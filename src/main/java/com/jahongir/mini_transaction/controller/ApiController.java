10716d79bd779049b6f55adbe6a61bcd3ad98804
jahongir <atametov97@mail.ru>
2023-02-01 12:31:57 +0500

    BaseService interface and Generic ApiController abstract class are added!

---
+++ src/main/java/com/jahongir/mini_transaction/controller/ApiController.java
@@ -0,0 +1,16 @@
+package com.jahongir.mini_transaction.controller;
+
+import com.jahongir.mini_transaction.service.BaseService;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author jahongir
+ * @created 01/02/23 - 12:25
+ * @project Mini_transaction/IntelliJ IDEA
+ */
+@RequiredArgsConstructor
+public abstract class ApiController<S extends BaseService> {
+    protected final S service;
+    protected final String API = "/api";
+    protected final String V1 = "/v1";
+}
---
+++ src/main/java/com/jahongir/mini_transaction/service/BaseService.java
@@ -0,0 +1,10 @@
+package com.jahongir.mini_transaction.service;
+
+/**
+ * @author jahongir
+ * @created 01/02/23 - 12:26
+ * @project Mini_transaction/IntelliJ IDEA
+ */
+
+public interface BaseService {
+}
